6540edc952c7c023dc13d4422c9d43f5ad37a80c
d-for-developer <sandytheshayar@gmail.com>
2022-03-06 12:07:01 +0530

    sticky navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,7 @@ import Logo from '../img/logo.png'
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="bg-gray-800">
+    <nav className="sticky top-0 z-50 bg-gray-800">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
